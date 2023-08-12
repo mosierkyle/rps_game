21d220ee02ffc7e44fbcc079bd3f3cd769e1974a
Kyle Mosier <mosierkyle@gmail.com>
2023-08-12 11:44:26 -0700

    Create function that plays rps game once
    Create the function that takes the user input and computer input as parameters and figures out who won, lost, or tied.

--- script.js
+++ script.js
@@ -15,9 +15,24 @@ function getComputersChoice() {
     }
 }
 
-// function that gets players choice 
-
 //function that plays the game 
+function rps(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!"
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win! Scissors beat Paper"
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You Lose! Rock beats Scissors"
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Win! Paper beats Rock"
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You Win! Rock beats Scissors"
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You Lose! Scissors beat Paper"
+    }
+}
 
 //function that plays multiple rounds of the game 
 
