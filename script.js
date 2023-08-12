c774653c318602b1a787308313b6a989479bc13e
Kyle Mosier <mosierkyle@gmail.com>
2023-08-12 12:35:06 -0700

    Create game function that plays rps game ${num} amount of times
    Created a function that will play the rock paper scissors game num amount of times and determine whether the player or the computer was the winner

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // function that will randomly return rock paper or scissors
-    
 function getComputersChoice() {
     let ran = Math.floor((Math.random() * (3)) + 1);
     switch(ran) {
@@ -15,26 +14,62 @@ function getComputersChoice() {
     }
 }
 
+//function that gets player input and returns it lowercase
+function getPlayer(){
+    let answer = prompt("Enter Rock, Paper, or Scissors?");
+    answer = answer.toLowerCase()
+    if (answer == "rock" || answer == "scissors" || answer == "paper")  {
+        return answer
+    } else getPlayer()
+}
+
 //function that plays the game 
 function rps(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "It's a tie!"
+        console.log("It's a tie!")
+        return "no one"
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You Lose! Paper beats Rock"
+        console.log("You Lose! Paper beats Rock")
+        return "computer"
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You Win! Scissors beat Paper"
+        console.log("You Win! Scissors beat Paper")
+        return "player"
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You Lose! Rock beats Scissors"
+        console.log("You Lose! Rock beats Scissors")
+        return "computer"
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You Win! Paper beats Rock"
+        console.log("You Win! Paper beats Rock")
+        return "player"
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You Win! Rock beats Scissors"
+        console.log("You Win! Rock beats Scissors")
+        return "player"
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You Lose! Scissors beat Paper"
+        console.log("You Lose! Scissors beat Paper")
+        return "computer"
     }
 }
 
 //function that plays multiple rounds of the game 
+function game(num) {
+    let playerWins = 0;
+    let computerWins = 0;
+    for(let i = 0; i < num; i++) {
+        let winner = rps(getPlayer(),getComputersChoice())
+        if(winner == "player") {
+            playerWins += 1;
+        } else if(winner == "computer") {
+            computerWins += 1;
+        }
+        console.log(`${playerWins} to ${computerWins}`);
+    }
+    if (playerWins > computerWins) {
+        console.log(`YOU WIN! ${playerWins} to ${computerWins}`);
+    } else if (playerWins < computerWins) {
+        console.log(`YOU LOSE! ${playerWins} to ${computerWins}`);
+    } else {
+        console.log(`Its a draw! ${playerWins} to ${computerWins}`)
+    }
+}
 
 //Calling the function 
-console.log(getComputersChoice());
+game(5);
