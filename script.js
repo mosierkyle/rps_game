64e6a229d0ed400ead20b27cb195620766a5a27a
Kyle Mosier <mosierkyle@gmail.com>
2023-08-14 11:48:19 -0700

    Minimize code and make game functioning with ui
    Did way too much here should've commited more often

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 // function that will randomly return rock paper or scissors
+let maxScore;
+let winner;
+let pWins = 0;
+let cWins = 0;
+let round = 1;
+
+const rockBtn = document.getElementById('rock');
+const paperBtn = document.getElementById('paper');
+const scissorsBtn = document.getElementById('scissors');
+
+function getRounds(){
+    return Number(document.querySelector('#round-input').value);
+  };
+
+
 function getComputersChoice() {
     let ran = Math.floor((Math.random() * (3)) + 1);
     switch(ran) {
@@ -14,79 +29,86 @@ function getComputersChoice() {
     }
 }
 
-//function that gets player input and returns it lowercase
-function getPlayer(){
-    let answer = prompt("Enter Rock, Paper, or Scissors?");
-    answer = answer.toLowerCase()
-    if (answer == "rock" || answer == "scissors" || answer == "paper")  {
-        return answer
-    } else getPlayer()
-}
+function viewStart() {
+    document.getElementById("rounds-div").style.display= "none";
+    document.getElementById("buttons-div").style.display= "flex";
+    document.getElementById("score-div").style.display= "flex"; 
+};
 
 //function that plays the game 
 function rps(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        console.log("It's a tie!")
+        document.querySelector('#round-result-p').textContent = "It's a tie!"
         return "no one"
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log("You Lose! Paper beats Rock")
+        document.querySelector('#round-result-p').textContent = "You Lose! Paper beats Rock"
         return "computer"
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        console.log("You Win! Scissors beat Paper")
+        document.querySelector('#round-result-p').textContent = "You Win! Scissors beat Paper"
         return "player"
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        console.log("You Lose! Rock beats Scissors")
+        document.querySelector('#round-result-p').textContent = "You Lose! Rock beats Scissors"
         return "computer"
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log("You Win! Paper beats Rock")
+        document.querySelector('#round-result-p').textContent = "You Win! Paper beats Rock"
         return "player"
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log("You Win! Rock beats Scissors")
+        document.querySelector('#round-result-p').textContent = "You Win! Rock beats Scissors"
         return "player"
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        console.log("You Lose! Scissors beat Paper")
+        document.querySelector('#round-result-p').textContent = "You Lose! Scissors beat Paper"
         return "computer"
     }
 }
 
-//function that plays multiple rounds of the game 
-function game(num) {
-    let playerWins = 0;
-    let computerWins = 0;
-    for(let i = 0; i < num; i++) {
-        let winner = rps(getPlayer(),getComputersChoice())
-        if(winner == "player") {
-            playerWins += 1;
-            console.log(`Score: ${playerWins} to ${computerWins}`);
-        } else if(winner == "computer") {
-            computerWins += 1;
-            console.log(`Score: ${playerWins} to ${computerWins}`);
-        } else {
-            num += 1;
-            console.log(`Still ${playerWins} to ${computerWins}`);
-        }
-    }
-    if (playerWins > computerWins) {
-        console.log(`YOU WIN! ${playerWins} to ${computerWins}`);
-    } else if (playerWins < computerWins) {
-        console.log(`YOU LOSE! ${playerWins} to ${computerWins}`);
+function whoWins() {
+    if (pWins >= maxScore) {
+        document.querySelector('#result').textContent = `YOU WIN!`;
+        document.getElementById("buttons-div").style.display= "none";
+    } else if (maxScore <= cWins) {
+        document.querySelector('#result').textContent = `YOU LOSE!`;
+        document.getElementById("buttons-div").style.display= "none";
     } else {
-        console.log(`Its a draw! ${playerWins} to ${computerWins}`)
+        return;
     }
 }
 
-//function that gets the amount of rounds the user wants to play;
-let roundBtn = document.querySelector('.round-button')
-roundBtn.addEventListener('click', () => {
-    let rounds = Number(document.querySelector('#round-input').value);
-    console.log(rounds);
-  });
+function updatePage(winner){
+    if(winner == "player") {
+        pWins += 1;
+        document.querySelector('#player-score').textContent = `Player: ${pWins}`;
+        round +=1
+    } else if(winner == "computer") {
+        cWins += 1;
+        document.querySelector('#computer-score').textContent = `Computer: ${cWins}`;
+        round += 1
+    } else {
+        round +=1
+    }
+}
 
-  
+function startGame() {
+    viewStart()
+    maxScore = getRounds();
+}
 
+function gameRock() {
+    let winner = rps('rock',getComputersChoice())
+    updatePage(winner)
+    document.querySelector('#what-round').textContent = `ROUND ${round}`;
+    whoWins()
+}
 
+function gamePaper() {
+    let winner = rps('paper',getComputersChoice())
+    updatePage(winner)
+    document.querySelector('#what-round').textContent = `ROUND ${round}`;
+    whoWins()
+}
 
-
-
-
-
+function gameScissors() {
+    let winner = rps('scissors',getComputersChoice())
+    updatePage(winner)
+    document.querySelector('#what-round').textContent = `ROUND ${round}`;
+    whoWins()
+}
